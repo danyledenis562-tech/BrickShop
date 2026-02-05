b02f2f50186a1723676b5b7f1c5c1eac8d18c37a
danyledenis562-tech <danyledenis562@gmail.com>
2026-02-05 09:35:34 +0100

    Remove boilerplate comments

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::get('/locale/{locale}', [LocaleController::class, 'switch'])->name('locale.switch');
 
